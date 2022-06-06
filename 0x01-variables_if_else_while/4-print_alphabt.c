6a0b2d23f45a93aa1ad04f95715c143b986a7d83
aboubakarsoulama <totypopito306@gmail.com>
2022-06-06 10:51:56 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,16 +8,15 @@
 int main(void)
 {
 int ch;
-
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 if (ch != 'e' && ch != 'q')
 {
-															putchar(ch);
-															}
-else                                    
+putchar(ch);
+}
+else
 {
-															}					
+}
 }
 putchar('\n');
 return (0);
