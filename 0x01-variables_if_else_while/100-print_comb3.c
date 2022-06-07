4a42394627c15add87376d028a05e92e1c893e7c
aboubakarsoulama <totypopito306@gmail.com>
2022-06-06 19:14:36 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,9 @@ int main(void)
 int c, i;
 
 for (c = '0'; c <= '9'; c++)
-{
+					
+			                                                                                                                                                                                                                                                                          
+{                                                                                                                                                                                                                                                                                                              
 for (i = '0'; i <= '9'; i++)
 {
 															if (c < i)
@@ -18,14 +20,13 @@ for (i = '0'; i <= '9'; i++)
 															putchar(c);
 															putchar(i);
 
-															if (c != '8' || (c == '8' && i != '9'))
+																														if (c != '8' || (c == '8' && i != '9'))
 																														{
 																														putchar(',');
 																																													putchar(' ');
 																																													}
 																														}
-																	
-}
+															}
 }
 putchar('\n');
 return (0);
