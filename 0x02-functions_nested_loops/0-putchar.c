807260227ff405d077a4aae46b39351c7947afb1
aboubakarsoulama <totypopito306@gmail.com>
2022-06-15 15:10:50 -0700

    2-largest_number.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,17 @@
 #include "main.h"
-/**
- * main - prints putchar
+/** main - prints putchar
  * Return: Always 0
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('a');
-_putchar('r');
-_putchar('u');
-return (0)
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
+return (0);
 }
